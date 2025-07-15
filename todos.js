9e63674b7f9772c23047704d622dae224d70bb00
JY <133961210+JYJE0N@users.noreply.github.com>
2025-07-15 14:21:24 +0900

    Create todos.js
    모듈 뭐냐

---
+++ todos.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require("mongodb");
+
+const client = new MongoClient(process.env.MONGO_URL);
+let todos;
+
+(async () => {
+  await client.connect();
+  console.log("✅ MongoDB Connected");
+  const db = client.db("doomock"); 
+  todos = db.collection("todos");
+})();
+
+exports.addTodo = async function(userId, task) {
+  await todos.insertOne({ userId, task, done: false, createdAt: new Date() });
+};
+
+exports.getTodos = async function(userId) {
+  return await todos.find({ userId }).toArray();
+};
+
+exports.clearTodos = async function(userId) {
+  await todos.deleteMany({ userId });
+};
