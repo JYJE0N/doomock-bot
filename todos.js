58ee60baffad166d30dd0b5483c0ed10843ca829
JY <133961210+JYJE0N@users.noreply.github.com>
2025-07-15 14:42:24 +0900

    Update todos.js
    무료로 하라잖니

--- todos.js
+++ todos.js
@@ -1,25 +1,25 @@
 const { MongoClient } = require("mongodb");
 
-console.log("DEBUG MONGO_URL:", process.env.MONGO_PUBLIC_URL);
+console.log("DEBUG MONGO_URL:", process.env.MONGO_URL);
 
-const client = new MongoClient(process.env.MONGO_PUBLIC_URL); // ✅ 이렇게 고쳐야 해
+const client = new MongoClient(process.env.MONGO_URL); // 🌟 이렇게 그냥 해도 왜
 let todos;
 
 (async () => {
-  await client.connect();
-  console.log("✅ MongoDB Connected");
-  const db = client.db("doomock");
-  todos = db.collection("todos");
+ await client.connect();
+ console.log("✅ MongoDB Connected");
+ const db = client.db("doomock");
+ todos = db.collection("todos");
 })();
 
 exports.addTodo = async function(userId, task) {
-  await todos.insertOne({ userId, task, done: false, createdAt: new Date() });
+ await todos.insertOne({ userId, task, done: false, createdAt: new Date() });
 };
 
 exports.getTodos = async function(userId) {
-  return await todos.find({ userId }).toArray();
+ return await todos.find({ userId }).toArray();
 };
 
 exports.clearTodos = async function(userId) {
-  await todos.deleteMany({ userId });
+ await todos.deleteMany({ userId });
 };
